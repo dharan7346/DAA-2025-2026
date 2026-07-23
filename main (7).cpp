95f7d03739dd057dac6aff9d4d6945c29ae293a1
dharan7346 <dharan.ponugubati129403@marwadiuniversity.ac.in>
2026-07-23 21:13:20 +0530

    LINEAR SEARCH

---
+++ main (7).cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, key;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int result = linearSearch(arr, n, key);
+
+    if (result == -1)
+        printf("Element not found.\n");
+    else
+        printf("Element found at index %d (Position %d).\n", result, result + 1);
+
+    return 0;
+}
